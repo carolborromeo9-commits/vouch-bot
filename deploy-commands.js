f7663aa9aec940a8a81d16a5a470aaab1fda5ae6
carolborromeo9-commits <carolborromeo9@gmail.com>
2026-04-09 00:51:13 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -5,14 +5,14 @@ const commands = [
   new SlashCommandBuilder()
     .setName('vouch')
     .setDescription('Send a vouch')
-    .addStringOption(option =>
-      option.setName('a_item').setDescription('Item').setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('b_feedback').setDescription('Feedback').setRequired(true)
-    )
     .addAttachmentOption(option =>
-      option.setName('c_proof').setDescription('Proof').setRequired(true)
+      option.setName('a_proof').setDescription('Proof').setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('b_item').setDescription('Item').setRequired(true)
+    )
+    .addStringOption(option =>
+      option.setName('c_feedback').setDescription('Feedback').setRequired(true)
     )
     .toJSON()
 ];
@@ -20,11 +20,16 @@ const commands = [
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
-  await rest.put(
-    Routes.applicationGuildCommands(
-      process.env.CLIENT_ID,
-      process.env.GUILD_ID
-    ),
-    { body: commands }
-  );
+  try {
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
+    console.log('Commands registered!');
+  } catch (error) {
+    console.error(error);
+  }
 })();
