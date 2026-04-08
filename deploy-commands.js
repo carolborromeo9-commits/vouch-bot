89e8ee0505c538fe391a5a2503549495c2d1b529
carolborromeo9-commits <carolborromeo9@gmail.com>
2026-04-09 02:32:29 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,42 +1,25 @@
 require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
-
-const commands = [
-  new SlashCommandBuilder()
-    .setName('vouch')
-    .setDescription('Send a vouch')
-    .addAttachmentOption(option =>
-      option.setName('proof').setDescription('Proof').setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('item').setDescription('Item').setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('feedback').setDescription('Feedback').setRequired(true)
-    )
-    .toJSON()
-];
+const { REST, Routes } = require('discord.js');
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
   try {
-    // delete all GLOBAL commands
+    // ❌ DELETE LAHAT (GLOBAL + GUILD)
     await rest.put(
       Routes.applicationCommands(process.env.CLIENT_ID),
       { body: [] }
     );
 
-    // replace GUILD commands with only this one
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
         process.env.GUILD_ID
       ),
-      { body: commands }
+      { body: [] }
     );
 
-    console.log('Global commands cleared, guild command registered.');
+    console.log('ALL COMMANDS DELETED');
   } catch (error) {
     console.error(error);
   }
