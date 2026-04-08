acdb34d32b87bf08ab57d6a84df9e96a4a8aefad
carolborromeo9-commits <carolborromeo9@gmail.com>
2026-04-09 02:21:45 +0800

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,34 +1,20 @@
 require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
-
-const commands = [
-  new SlashCommandBuilder()
-    .setName('vouch')
-    .setDescription('Send a vouch')
-    .addAttachmentOption(option =>
-      option.setName('proof').setDescription('Proof').setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('item').setDescription('Item').setRequired(true)
-    )
-    .addStringOption(option =>
-      option.setName('feedback').setDescription('Feedback').setRequired(true)
-    )
-    .toJSON()
-];
+const { REST, Routes } = require('discord.js');
 
 const rest = new REST({ version: '10' }).setToken(process.env.BOT_TOKEN);
 
 (async () => {
   try {
+    // ❌ DELETE ALL COMMANDS
     await rest.put(
       Routes.applicationGuildCommands(
         process.env.CLIENT_ID,
         process.env.GUILD_ID
       ),
-      { body: commands }
+      { body: [] }
     );
-    console.log('Commands registered!');
+
+    console.log('All commands deleted!');
   } catch (error) {
     console.error(error);
   }
